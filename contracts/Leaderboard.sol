a33d7ee2e863f096d97863abe8aa90c35c7f7f02
Jerry Lee <jerry.lee.tw.1992@gmail.com>
2019-10-20 14:32:25 +0800

    Fix the bugs

--- contracts/Leaderboard.sol
+++ contracts/Leaderboard.sol
@@ -5,28 +5,24 @@ contract Leaderboard {
     mapping (address => uint) public player_ETH;
     address public firstPlacePlayer = address(0);
 
-    function _updateFirstPlace() private{
-        for (uint i = 0; i < players.length ; i++) {
-            address player = players[i];
-            uint playerWinETH = player_ETH[player];
-            // The ETH that you get needs to be greater than the first place player who is in the first time.
-            if (playerWinETH > player_ETH[firstPlacePlayer]) {
-                firstPlacePlayer = player;
-            }
+    function _updateFirstPlace(address addr) private{
+        // The ETH that you get needs to be greater than the first place player who is in the first time.
+        if (player_ETH[addr] > player_ETH[firstPlacePlayer]) {
+            firstPlacePlayer = addr;
         }
     }
 
-    function enterLeaderBoard(address add, uint winAmount) internal {
-        if (!_isInLeaderBoard(add)) {
-            players.push(add);
+    function enterLeaderBoard(address addr, uint winAmount) internal {
+        if (!_isInLeaderBoard(addr)) {
+            players.push(addr);
         }
-        player_ETH[add] += winAmount;
-        _updateFirstPlace();
+        player_ETH[addr] += winAmount;
+        _updateFirstPlace(addr);
     }
 
-    function _isInLeaderBoard(address add) private view returns(bool) {
+    function _isInLeaderBoard(address addr) private view returns(bool) {
         for (uint i = 0; i < players.length ; i++) {
-            if (players[i] == add) {
+            if (players[i] == addr) {
                 return true;
             }
         }
